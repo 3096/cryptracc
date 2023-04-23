22f0c1342f5f03b5f81c3e44b2761087077b2baa
3096 <contact@the3096.com>
2023-04-22 19:01:46 -0700

    add wagmi

--- client/src/routes/RootPage.tsx
+++ client/src/routes/RootPage.tsx
@@ -3,14 +3,14 @@
 
 // import React, { useState, useEffect } from 'react'
 import { Button, Alert } from "@mui/material";
-import "./RootPage.css"
-import MetaMask_Fox from "../assets/MetaMask_Fox.svg"
+import "./RootPage.css";
+import MetaMask_Fox from "../assets/MetaMask_Fox.svg";
 
 export default function RootPage() {
   const metaMaskFoxIcon = {
     width: 40,
     height: 40,
-  }
+  };
 
   const metaMaskButtonStyle = {
     width: 300,
@@ -22,60 +22,118 @@ export default function RootPage() {
 
   return (
     <div className="rootPage">
-      <h1><strong>Let's get started</strong></h1>
+      <h1>
+        <strong>Let's get started</strong>
+      </h1>
 
       <div className="walletConnection">
         <h3>Connect your ETH wallet</h3>
 
-        <Button variant="contained" style={metaMaskButtonStyle} sx={{
-          textTransform: "none",
-          borderRadius: 3,
-          borderBottomLeftRadius: 0,
-          borderBottomRightRadius: 0,
-        }}>
+        <Button
+          variant="contained"
+          style={metaMaskButtonStyle}
+          sx={{
+            textTransform: "none",
+            borderRadius: 3,
+            borderBottomLeftRadius: 0,
+            borderBottomRightRadius: 0,
+          }}
+        >
           <img src={MetaMask_Fox} style={metaMaskFoxIcon} />
           <strong>Connect to METAMASK</strong>
         </Button>
 
-        <Alert variant="outlined" severity="info" sx={{
-          height: 40,
-          width: 266,
-          bgcolor: "none",
-          color: `#FFFFFF`,
-          borderRadius: 3,
-          borderTopLeftRadius: 0,
-          borderTopRightRadius: 0,
-          outlineWidth: 0,
-        }}>
-          <a href="https://metamask.io/download/"><strong>*<u>MetaMask</u></strong></a> is required.
+        <Alert
+          variant="outlined"
+          severity="info"
+          sx={{
+            height: 40,
+            width: 266,
+            bgcolor: "none",
+            color: `#FFFFFF`,
+            borderRadius: 3,
+            borderTopLeftRadius: 0,
+            borderTopRightRadius: 0,
+            outlineWidth: 0,
+          }}
+        >
+          <a href="https://metamask.io/download/">
+            <strong>
+              *<u>MetaMask</u>
+            </strong>
+          </a>{" "}
+          is required.
         </Alert>
       </div>
 
       <div className="idSubmission">
         <h3>Upload your ID</h3>
 
-        <Button variant="contained" component="label" sx={{
-          height: 50,
-          width: 150,
-          bgcolor: `#C8C8C8`,
-          color: `#000000`,
-          textTransform: "none",
-          borderRadius: 3,
-        }}>
+        <Button
+          variant="contained"
+          component="label"
+          sx={{
+            height: 50,
+            width: 150,
+            bgcolor: `#C8C8C8`,
+            color: `#000000`,
+            textTransform: "none",
+            borderRadius: 3,
+          }}
+        >
           Choose File
           <input hidden accept="image/*" multiple type="file" />
         </Button>
       </div>
 
-      <Button variant="contained" component="label"sx={{
-        height: 50,
-        width: 250,
-        bgcolor: `#30B46C`,
-        color: `#FFFFFF`,
-        borderRadius: 3,
-      }}>
+      <Button
+        variant="contained"
+        component="label"
+        sx={{
+          height: 50,
+          width: 250,
+          bgcolor: `#30B46C`,
+          color: `#FFFFFF`,
+          borderRadius: 3,
+        }}
+      >
         <strong>CONTINUE</strong>
       </Button>
     </div>
   );
 }
+// import Button from "@mui/material/Button";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import Typography from "@mui/material/Typography";
+// import { useAccount, useConnect, useDisconnect } from "wagmi";
+// import { InjectedConnector } from "wagmi/connectors/injected";
+
+// export default function RootPage() {
+//   const { address, isConnected } = useAccount();
+//   const { connect } = useConnect({
+//     connector: new InjectedConnector(),
+//   });
+//   const { disconnect } = useDisconnect();
+
+//   return (
+//     <>
+//       {isConnected ? (
+//         <div>
+//           <Card>
+//             <CardContent>
+//               <Typography my={1}>Connected to {address}</Typography>
+//               <Button variant="contained" onClick={() => disconnect()}>
+//                 Disconnect
+//               </Button>
+//             </CardContent>
+//           </Card>
+//         </div>
+//       ) : (
+//         <Button variant="contained" onClick={() => connect()}>
+//           Connect Wallet
+//         </Button>
+//       )}
+//     </>
+//   );
+// }
