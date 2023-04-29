486ec69e97a0905a44770a36ccbcbdbe26ab40e9
TechGeek-Mohit <themohit2002@gmail.com>
2023-04-28 19:00:48 -0700

    Dashboard MVP version

--- client/src/routes/DashboardPage.tsx
+++ client/src/routes/DashboardPage.tsx
@@ -1,4 +1,12 @@
+import React from 'react';
 import {useState} from 'react' ;
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+
+
 
 
 export default function DashboardPage() {  
@@ -11,57 +19,29 @@ export default function DashboardPage() {
   return(
     <div>
       <img className="avatar" />
-      <AboutPage/>
-      <App/>
       <Profile/>
-      <ShoppingList/>
-      <div>
-      <h1>Counters that update seperately</h1>
-        <MyButton count={count} onClick={handleClick} />
-        <MyButton count={count} onClick={handleClick} />
-      </div>
+      <App/>
     </div>
   );
 }
 
+//Name of the user
+//Later on we will get userName from the signup/login page
 const user = {
   name: 'Hedy Lamarr',
   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
+  imageSize: 200,
 };
 
-/*
-UI for Contract button
-*/
-// function CreateContractButton() {
-//   function handleClick() {
-//     alert('Add Contract');
-//   }
-
-//   return (
-//     <button onClick={handleClick}>
-//       New Contract +
-//     </button>
-//   );
-// }
 
 /*
 Information about the user
 */
-function AboutPage(){
+function Profile(){
   return(
     <>
       <h1>CryptAcc</h1>
-      <p>Welcome to {user.name} to CryptAcc's DashBoard</p>
-    </>
-  );
-}
-
-
-export function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
+      <p>Welcome <b>{user.name} </b> to CryptAcc's DashBoard</p>
       <img
         className="avatar"
         src={user.imageUrl}
@@ -71,36 +51,16 @@ export function Profile() {
           height: user.imageSize
         }}
       />
+      
     </>
   );
 }
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export function ShoppingList() {
-  const listItems = products.map(product => 
-  <li key={product.id}> {product.title} </li>)
-  return (
-    <ul>{listItems}</ul>
-  );
-}
-
-function MyButton({ count, onClick }) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-
-}
 
 function App() {
   const [users, setUsers] = useState([]);
   const [showForm, setShowForm] = useState(false);
+  const [name, setName] = useState("");
   const [id1, setid1] = useState("");
   const [id2, setid2] = useState("");
   const [sign1, setsign1] = useState("");
@@ -111,6 +71,7 @@ function App() {
     e.preventDefault();
     const formData = new FormData(e.target);
     const user = {
+      name : name,
       id1: id1,
       id2: id2,
       sign1: sign1,
@@ -122,9 +83,10 @@ function App() {
     console.log(users);
 
     // add the new user object to the users array
-    setUsers(prevState => [...prevState, user])
+    setUsers(prevState => [...prevState, user]);
 
     // reset the form fields and close the form
+    setName("");
     setid1("");
     setid2("");
     setsign1("");
@@ -139,54 +101,65 @@ function App() {
   }
 
   return (
-  <>
+  <div>
+    <button onClick={() => setShowForm(true)}>Add Contract +</button>
+    {showForm && (
+      <div className="form-popup">
+        <form onSubmit={handleSubmit}>
+        <br />
+          <label>
+            Contract Name:
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Choose a file:
+            <input type="file" onChange={handleFileChange} />
+          </label>
+          <br />
+          <label>
+            user1 HashId:
+            <input type="text" value={id1} onChange={e => setid1(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            user2 HashId:
+            <input type="text" value={id2} onChange={e => setid2(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Signature_User1:
+            <textarea value={sign1} onChange={e => setsign1(e.target.value)} />
+          </label>
+          <label>
+            <br />
+            Signature_User2:
+            <textarea value={sign2} onChange={e => setsign2(e.target.value)} />
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+          <button onClick={() => setShowForm(false)}>Close</button>
+        </form>
+      </div>
+    )}
     <div>
-      <button onClick={() => setShowForm(true)}>Add Contract +</button>
-      {showForm && (
-        <div className="form-popup">
-          <form onSubmit={handleSubmit}>
-            <label>
-              Choose a file:
-              <input type="file" onChange={handleFileChange} />
-            </label>
-            <br />
-            <label>
-              user1:
-              <input type="text" value={id1} onChange={e => setid1(e.target.value)} />
-            </label>
-            <br />
-            <label>
-              user2:
-              <input type="text" value={id2} onChange={e => setid2(e.target.value)} />
-            </label>
-            <br />
-            <label>
-              Signature_User1:
-              <textarea value={sign1} onChange={e => setsign1(e.target.value)} />
-            </label>
-            <label>
-              <br />
-              Signature_User2:
-              <textarea value={sign2} onChange={e => setsign2(e.target.value)} />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-            <button onClick={() => setShowForm(false)}>Close</button>
-          </form>
-        </div>
-      )}
-    {users.map((user, index) => (
-        <div key={index} className="user-box">
-          <p>ID1: {user.id1}</p>
-          <p>ID2: {user.id2}</p>
-          <p>Signature_User1: {user.sign1}</p>
-          <p>Signature_User2: {user.sign2}</p>
-          <p>File: {user.file.name}</p>
-        </div>
+      <h3>Contracts Signed</h3>
+      {users.map((user, index) => (
+        <Box key={index} sx={{ backgroundColor: "#eee", padding: 5 }}>
+          <Typography variant="h7" display="inline">Contract Name: </Typography>
+          <Typography variant="h7" display="inline">{user.name}</Typography>
+          <br />
+          <Typography variant="h7" display="inline">user1 HashId: </Typography>
+          <Typography variant="h7" display="inline">{user.id1}</Typography>
+          <br />
+          <Typography variant="h7" display="inline">user2 HashId: </Typography>
+          <Typography variant="h7" display="inline">{user.id2}</Typography>
+          <br />
+        </Box>
       ))}
-
     </div>
-  </>
+    
+  </div>
   );
 }
 
