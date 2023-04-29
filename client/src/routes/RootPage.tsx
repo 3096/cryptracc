b4fef6b169fbffb61e4e3f09ff81973a6fd3c4dc
3096 <contact@the3096.com>
2023-04-28 20:40:15 -0700

    add wallet connect functionality

--- client/src/routes/RootPage.tsx
+++ client/src/routes/RootPage.tsx
@@ -3,58 +3,42 @@
 import { Button } from "@mui/material";
 import "./RootPage.css";
 import WalletConnectButton from "../components/WalletConnectButton";
+import { useAccount } from "wagmi";
+import { useNavigate } from "react-router-dom";
 
 export default function RootPage() {
+  const { isConnected } = useAccount();
+  const navigate = useNavigate();
+
+  if (isConnected) {
+    navigate("/dashboard");
+  }
+
   return (
     <div className="rootPage">
-      <center><h1><strong>Let's get started</strong></h1></center>
+      <center>
+        <h1>
+          <strong>Let's get started</strong>
+        </h1>
+      </center>
 
       <WalletConnectButton />
 
-      <center><Button variant="contained" component="label" sx={{
-          height: 50,
-          width: 250,
-          bgcolor: `#30B46C`,
-          color: `#FFFFFF`,
-          borderRadius: 3,
-        }}>
-        <strong>CONTINUE</strong>
-      </Button></center>
+      <center>
+        <Button
+          variant="contained"
+          component="label"
+          sx={{
+            height: 50,
+            width: 250,
+            bgcolor: `#30B46C`,
+            color: `#FFFFFF`,
+            borderRadius: 3,
+          }}
+        >
+          <strong>CONTINUE</strong>
+        </Button>
+      </center>
     </div>
   );
 }
-// import Button from "@mui/material/Button";
-// import Card from "@mui/material/Card";
-// import CardContent from "@mui/material/CardContent";
-// import Typography from "@mui/material/Typography";
-// import { useAccount, useConnect, useDisconnect } from "wagmi";
-// import { InjectedConnector } from "wagmi/connectors/injected";
-
-// export default function RootPage() {
-//   const { address, isConnected } = useAccount();
-//   const { connect } = useConnect({
-//     connector: new InjectedConnector(),
-//   });
-//   const { disconnect } = useDisconnect();
-
-//   return (
-//     <>
-//       {isConnected ? (
-//         <div>
-//           <Card>
-//             <CardContent>
-//               <Typography my={1}>Connected to {address}</Typography>
-//               <Button variant="contained" onClick={() => disconnect()}>
-//                 Disconnect
-//               </Button>
-//             </CardContent>
-//           </Card>
-//         </div>
-//       ) : (
-//         <Button variant="contained" onClick={() => connect()}>
-//           Connect Wallet
-//         </Button>
-//       )}
-//     </>
-//   );
-// }
