88cbc6fbfdd7cfccd54e5170b623aead7bc3b9dc
3096 <contact@the3096.com>
2023-04-28 22:17:49 -0700

    further strip down file hasher

--- client/src/components/FileHashing.tsx
+++ client/src/components/FileHashing.tsx
@@ -1,54 +1,41 @@
-import React, { useState } from 'react';
-import { sha256} from 'crypto-hash';
-import './FileHashing.css';
+import { sha256 } from "crypto-hash";
+import "./FileHashing.css";
+import { Dispatch } from "react";
 
-export default function FileHashing() {
-    let [file_input, setFileInput] = useState('');
-    let [output, setOutput] = useState('');
+type Props = { setOutput: Dispatch<React.SetStateAction<string>> };
 
-    // For handling file input
-    const handleFileInput = (e : any) => {
-        // Initializing the file reader
-        const fr = new FileReader();
+export default function FileHashing({ setOutput }: Props) {
+  // For handling file input
+  const handleFileInput = (e: any) => {
+    // Initializing the file reader
+    const fr = new FileReader();
 
-        // Listening to when the file has been read.
-        fr.onload = async () => {
-            let result = '';
-            // Hashing the content based on the active algorithm
-            result = await sha256(fr.result as string);
+    // Listening to when the file has been read.
+    fr.onload = async () => {
+      let result = "";
+      // Hashing the content based on the active algorithm
+      result = await sha256(fr.result as string);
 
+      // Setting the hashed text as the output
+      setOutput("0x" + result);
+    };
 
-            // Setting the hashed text as the output
-            setOutput(result);
-            // Setting the content of the file as file input
-            setFileInput(fr.result as string);
-        }
+    // Reading the file.
+    fr.readAsArrayBuffer(e.target.files[0]);
+  };
 
-        // Reading the file.
-        fr.readAsArrayBuffer(e.target.files[0]);
-    }
-
-    return (
-        <div className='hashing-container'>
-            <div className='hashing-content'>
-                <div className="hashing-form">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="file-input">File Input</label>
-                            <input type="file" className="form-control" id="file-input" onChange={handleFileInput} />
-                        </div>
-                    </form>
-                </div>
-
-                <div className="hashed-output">
-                    <h4 className="hashed-algorithm-heading">Output</h4>
-                    <div className="hashed-algorithm-container">
-                        <p className="hashed-algorithm-text">
-                            {output}
-                        </p>
-                    </div>
-                </div>
+  return (
+    <div className="hashing-container">
+      <div className="hashing-content">
+        <div className="hashing-form">
+          <form>
+            <div className="form-group">
+              <label htmlFor="file-input">File Input</label>
+              <input type="file" className="form-control" id="file-input" onChange={handleFileInput} />
             </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
